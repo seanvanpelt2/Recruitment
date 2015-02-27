149070f2eea1c2901ed158ea8217d44ae2b369f3
seanvanpelt2 <seanvanpelt2@gmail.com>
2015-02-27 11:06:35 -0500

    App skeleton
    The start of the app

--- Recruitment/ViewController.swift
+++ Recruitment/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //specifying special things for the app
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var IdTextField: UITextField!
+    
+    @IBAction func IdSubmit(sender: UIButton) { //Button for submiting the ID to login
+        
+        //insert code for testing their ID against the database to see who it is and log them in. Then go to the 'Admin Menu' page
+        
+        /*
+        if (id is found in database) {
+            self.performSegueWithIdentifier("To-AdminMenu", sender: self)
+        } else {
+            self.messageLabel.text = "ID not found.";
+        }
+        */
+        
+        //used for testing. Remove once you get ID check working
+        self.performSegueWithIdentifier("To-AdminMenu", sender: self)
+        
+    }
 }
 
